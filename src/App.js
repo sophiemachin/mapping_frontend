03324109b4189592116bb13adcd60af9a0b8fb37
sem <sem@pearson.co.uk>
2020-06-24 19:35:02 +0100

    App: add toolbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Map from './Map'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 function App() {
   return (
-    <div>
-      <h1>Sophie's mapping stuff</h1>
+    <div style={{marginTop: '100px'}}>
+      <AppBar>
+        <Toolbar>
+          <h1>Sophie's mapping stuff</h1>
+        </Toolbar>
+      </AppBar>
+
       <Map />
     </div>
   );
