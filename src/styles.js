a1c42d518d87dced044a4ac5f9c1b2c86abbfc9e
sem <sem@pearson.co.uk>
2020-07-28 19:39:20 +0100

    styles: add

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: { main: 'rgb(163,0,0)' },
+    secondary: { main: 'rgb(0,175,181)' },
+    text : {
+      primary : 'rgb(230,230,230',
+    }
+  },
+})
